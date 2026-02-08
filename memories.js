4e200ec838737e00b3a653dfcacb7a29524fc486
adityaShivani <abhipachauri577569@gmail.com>
2026-02-08 14:36:53 +0530

    Add files via upload

--- memories.js
+++ memories.js
@@ -1,5 +1,33 @@
+// const memories = document.querySelectorAll(".memory");
+// const btn = document.getElementById("continue");
+
+// function reveal(){
+//   let visibleCount=0;
+
+//   memories.forEach(m=>{
+//     const rect=m.getBoundingClientRect();
+
+//     if(rect.top < window.innerHeight - 80){
+//       m.classList.add("show");
+//       visibleCount++;
+//     }
+//   });
+
+//   if(visibleCount===memories.length){
+//     btn.style.display="block";
+//   }
+// }
+
+// window.addEventListener("scroll",reveal);
+// reveal();
+
+// btn.onclick=()=>{
+//   window.location.href="valentine.html";
+// };
+
+
 const memories = document.querySelectorAll(".memory");
-const btn = document.getElementById("continue");
+const btnContainer = document.getElementById("dayButtons");
 
 function reveal(){
   let visibleCount=0;
@@ -14,13 +42,13 @@ function reveal(){
   });
 
   if(visibleCount===memories.length){
-    btn.style.display="block";
+    btnContainer.style.display="block";
   }
 }
 
 window.addEventListener("scroll",reveal);
 reveal();
 
-btn.onclick=()=>{
-  window.location.href="valentine.html";
-};
+function openDay(page){
+  window.location.href = page;
+}
